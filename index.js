32f808b420d772872f1c44e37fb6aa1bde263295
gabrielbarbosaa1000 <gabriel.alb@hotmail.com>
2025-06-04 16:23:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // ======================================
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1000;
 const fs = require('fs');
 const path = require('path');
 const qrcode = require('qrcode');
